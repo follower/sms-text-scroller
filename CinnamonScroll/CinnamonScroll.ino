0eebaf1b97f1360d8a4e202152325dcf611a7550
follower <follower@rancidbacon.com>
2012-05-16 16:08:44 +1200

    Add rough demo that shows flicker-free operation with the ability to send data from the android device to the Arduino.

--- CinnamonScroll/CinnamonScroll.ino
+++ CinnamonScroll/CinnamonScroll.ino
@@ -1,7 +1,73 @@
+// This is written for my version 1.x USB_Host_Shield library
+// with an NAK_LIMIT edit.
+
+// This is currently a bodge of parts of the DMD demo and a USB_Host_Shield demo.
+
+#include <Max3421e.h>
+#include <Usb.h>
+#include <AndroidAccessory.h>
+
+#include <SPI.h>        //SPI.h must be included as DMD is written by SPI (the IDE complains otherwise)
+#include <DMD.h>        //
+#include <TimerOne.h>   //
+#include "SystemFont5x7.h"
+#include "Arial_black_16.h"
+
+//Fire up the DMD library as dmd
+#define DISPLAYS_ACROSS 2
+#define DISPLAYS_DOWN 1
+DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
+
+
+AndroidAccessory acc("Circuits@Home, ltd.",
+            "USB Host Shield",
+            "Arduino Terminal for Android",
+            "1.0",
+            "http://www.circuitsathome.com",
+            "0000000000000001");
+
+
+void ScanDMD() {
+  // Interrupt handler for Timer1 (TimerOne) driven DMD refresh scanning, this gets
+  // called at the period set in Timer1.initialize();
+  dmd.scanDisplayBySPI();
+}
+
+
 void setup() {
 
+  acc.powerOn();
+
+   //initialize TimerOne's interrupt/CPU usage used to scan and refresh the display
+   Timer1.initialize( 5000 );           //period in microseconds to call ScanDMD. Anything longer than 5000 (5ms) and you can see flicker.
+   Timer1.attachInterrupt( ScanDMD );   //attach the Timer1 interrupt to ScanDMD which goes to dmd.scanDisplayBySPI()
+
+   //clear/init the DMD pixels held in RAM
+   dmd.clearScreen( true );   //true is normal (all pixels off), false is negative (all pixels on)
 }
 
 void loop() {
-  
+
+    dmd.clearScreen( true );
+    dmd.selectFont(Arial_Black_16);
+
+    int xpos = 1;
+    dmd.drawChar(  0,  3, ':', GRAPHICS_NORMAL );
+
+    while (1) {
+
+      if (acc.isConnected()) {
+        while(acc.available() > 0) {
+            dmd.drawChar(  (xpos++) * 10,  3, (char) acc.read(), GRAPHICS_NORMAL );
+          }
+      }
+
+
+      unsigned long timer = millis() + 100;
+
+      while (millis() < timer) {
+      }
+
+    }
+
 }
